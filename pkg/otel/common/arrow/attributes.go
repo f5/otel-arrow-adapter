e4445d0df55ef410fe921ee710279072379a4a97
Laurent Querel <laurent.querel@gmail.com>
2022-11-01 14:36:05 -0700

    Attributes based on any value builder

--- pkg/otel/common/arrow/attributes.go
+++ pkg/otel/common/arrow/attributes.go
@@ -24,13 +24,7 @@ type AttributesBuilder struct {
 
 	builder *array.MapBuilder              // map builder
 	kb      *array.BinaryDictionaryBuilder // key builder
-	ib      *array.SparseUnionBuilder      // item builder
-
-	strBuilder    *array.BinaryDictionaryBuilder
-	i64Builder    *array.Int64Builder
-	f64Builder    *array.Float64Builder
-	boolBuilder   *array.BooleanBuilder
-	binaryBuilder *array.BinaryDictionaryBuilder
+	ib      *AnyValueBuilder               // item any value builder
 }
 
 // NewAttributesBuilder creates a new AttributesBuilder with a given allocator.
@@ -44,23 +38,13 @@ func NewAttributesBuilder(pool *memory.GoAllocator) *AttributesBuilder {
 
 // AttributesBuilderFrom creates a new AttributesBuilder from an existing MapBuilder.
 func AttributesBuilderFrom(mb *array.MapBuilder) *AttributesBuilder {
-	ib := mb.ItemBuilder().(*array.SparseUnionBuilder)
-	strBuilder := ib.Child(0).(*array.BinaryDictionaryBuilder)
-	intBuilder := ib.Child(1).(*array.Int64Builder)
-	doubleBuilder := ib.Child(2).(*array.Float64Builder)
-	boolBuilder := ib.Child(3).(*array.BooleanBuilder)
-	binaryBuilder := ib.Child(4).(*array.BinaryDictionaryBuilder)
+	ib := AnyValueBuilderFrom(mb.ItemBuilder().(*array.SparseUnionBuilder))
 
 	return &AttributesBuilder{
-		released:      false,
-		builder:       mb,
-		kb:            mb.KeyBuilder().(*array.BinaryDictionaryBuilder),
-		ib:            ib,
-		strBuilder:    strBuilder,
-		i64Builder:    intBuilder,
-		f64Builder:    doubleBuilder,
-		boolBuilder:   boolBuilder,
-		binaryBuilder: binaryBuilder,
+		released: false,
+		builder:  mb,
+		kb:       mb.KeyBuilder().(*array.BinaryDictionaryBuilder),
+		ib:       ib,
 	}
 }
 
@@ -106,24 +90,14 @@ func (b *AttributesBuilder) Append(attrs pcommon.Map) error {
 			return true
 		}
 
-		switch v.Type() {
-		case pcommon.ValueTypeEmpty:
-			b.append0Attrs()
-		case pcommon.ValueTypeStr:
-			err = b.appendStr(key, v.Str())
-		case pcommon.ValueTypeInt:
-			err = b.appendI64(key, v.Int())
-		case pcommon.ValueTypeDouble:
-			err = b.appendF64(key, v.Double())
-		case pcommon.ValueTypeBool:
-			err = b.appendBool(key, v.Bool())
-		case pcommon.ValueTypeBytes:
-			err = b.appendBinary(key, v.Bytes().AsRaw())
-		case pcommon.ValueTypeSlice:
-			err = fmt.Errorf("slice value type not supported")
-		case pcommon.ValueTypeMap:
-			err = fmt.Errorf("map value type not supported")
+		// Append the key
+		err := b.kb.AppendString(key)
+		if err != nil {
+			return false
 		}
+
+		// Append the value
+		err = b.ib.Append(v)
 		if err != nil {
 			return false
 		}
@@ -139,12 +113,6 @@ func (b *AttributesBuilder) Release() {
 		b.kb.Release()
 		b.ib.Release()
 
-		b.strBuilder.Release()
-		b.i64Builder.Release()
-		b.f64Builder.Release()
-		b.boolBuilder.Release()
-		b.binaryBuilder.Release()
-
 		b.released = true
 	}
 }
@@ -158,100 +126,4 @@ func (b *AttributesBuilder) appendNAttrs(count int) {
 // append0Attrs appends an empty set of key-value pairs to the builder.
 func (b *AttributesBuilder) append0Attrs() {
 	b.builder.AppendNull()
-}
-
-// appendStr appends a new string attribute to the builder.
-func (b *AttributesBuilder) appendStr(k string, v string) error {
-	err := b.kb.AppendString(k)
-	if err != nil {
-		return err
-	}
-	b.ib.Append(StrCode)
-	if v == "" {
-		b.strBuilder.AppendNull()
-	} else {
-		if err := b.strBuilder.AppendString(v); err != nil {
-			return err
-		}
-	}
-	b.i64Builder.AppendNull()
-	b.f64Builder.AppendNull()
-	b.boolBuilder.AppendNull()
-	b.binaryBuilder.AppendNull()
-
-	return nil
-}
-
-// appendI64 appends a new int64 attribute to the builder.
-func (b *AttributesBuilder) appendI64(k string, v int64) error {
-	err := b.kb.AppendString(k)
-	if err != nil {
-		return err
-	}
-	b.ib.Append(I64Code)
-	b.i64Builder.Append(v)
-
-	b.strBuilder.AppendNull()
-	b.f64Builder.AppendNull()
-	b.boolBuilder.AppendNull()
-	b.binaryBuilder.AppendNull()
-
-	return nil
-}
-
-// appendF64 appends a new double attribute to the builder.
-func (b *AttributesBuilder) appendF64(k string, v float64) error {
-	err := b.kb.AppendString(k)
-	if err != nil {
-		return err
-	}
-	b.ib.Append(F64Code)
-	b.f64Builder.Append(v)
-
-	b.strBuilder.AppendNull()
-	b.i64Builder.AppendNull()
-	b.boolBuilder.AppendNull()
-	b.binaryBuilder.AppendNull()
-
-	return nil
-}
-
-// appendBool appends a new bool attribute to the builder.
-func (b *AttributesBuilder) appendBool(k string, v bool) error {
-	err := b.kb.AppendString(k)
-	if err != nil {
-		return err
-	}
-	b.ib.Append(BoolCode)
-	b.boolBuilder.Append(v)
-
-	b.strBuilder.AppendNull()
-	b.i64Builder.AppendNull()
-	b.f64Builder.AppendNull()
-	b.binaryBuilder.AppendNull()
-
-	return nil
-}
-
-// appendBinary appends a new binary attribute to the builder.
-func (b *AttributesBuilder) appendBinary(k string, v []byte) error {
-	err := b.kb.AppendString(k)
-	if err != nil {
-		return err
-	}
-	b.ib.Append(BinaryCode)
-	if v == nil {
-		b.binaryBuilder.AppendNull()
-	} else {
-		if err := b.binaryBuilder.Append(v); err != nil {
-			return err
-		}
-	}
-
-	b.strBuilder.AppendNull()
-	b.i64Builder.AppendNull()
-	b.f64Builder.AppendNull()
-	b.boolBuilder.AppendNull()
-
-	return nil
 }
