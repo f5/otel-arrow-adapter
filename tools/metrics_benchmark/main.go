0733c1f5f9d3037284936385c6f4d99975d48d7f
Laurent Querel <l.querel@f5.com>
2023-01-19 18:18:55 -0800

    Update benchmark screenshots

--- tools/metrics_benchmark/main.go
+++ tools/metrics_benchmark/main.go
@@ -48,7 +48,7 @@ func main() {
 	// Performance comparison for each input file
 	for i := range inputFiles {
 		// Compare the performance between the standard OTLP representation and the OTLP Arrow representation.
-		profiler := benchmark.NewProfiler([]int{100, 1000, 2000, 5000, 10000}, "output/metrics_benchmark.log", warmUpIter)
+		profiler := benchmark.NewProfiler([]int{1, 10, 100, 1000, 2000}, "output/metrics_benchmark.log", warmUpIter)
 		compressionAlgo := benchmark.Zstd()
 		maxIter := uint64(3)
 		profiler.Printf("Dataset '%s'\n", inputFiles[i])
